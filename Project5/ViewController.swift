d168ca0f7bac13eb15608a0a15d7a3805c57ee08
rtmrshr <ratmir.sher@gmail.com>
2023-01-02 14:42:51 +0200

    Initial Commit

---
+++ Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by  ratmir on 02.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
